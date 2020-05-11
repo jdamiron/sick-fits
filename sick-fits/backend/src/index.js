351b6e82dee1b7c1dcc6e4e9976b282ad2bd8840
Jeremiah Damiron <jeremiahdamiron@Jeremiahs-MacBook-Pro.local>
2020-05-11 09:50:04 -0400

    Inital work on backend

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,22 @@
-// let's go!
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+//User express middleware to handle cookies (JWT)
+//User express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      prigin: process.env.FRONTEND_URL,
+    },
+  },
+  (deets) => {
+    console.log(
+      `Server is now running on port http://localhost: ${deets.port}`
+    );
+  }
+);
